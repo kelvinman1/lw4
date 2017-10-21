7553a012e7a8835bb938d41e67236332978be04b
Anonimnii <Anonimnii@aefaef.ef>
2017-10-21 14:13:59 +0300

    Добавил префикс к минифицированным файлам.
    Добавил префикс к минифицированным файлам.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
     gutil  = require('gulp-util');
   
 gulp.task('minify-js', function () {
   gulp.src('js/*.js')
   .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
   .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
   .pipe(gulp.dest('build/js'))
 });
 gulp.task('minify-css', function () {
   gulp.src('css/*.css')
   .pipe(cssmin())
+  .pipe(rename({suffix: '.min'}))
   .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
   .pipe(gulp.dest('build/css'))
 });
