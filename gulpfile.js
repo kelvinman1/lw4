43d393868dcc55a4c6adcf26ca40a5d87cd9594c
Anonimnii <Anonimnii@aefaef.ef>
2017-10-28 19:12:39 +0300

    Сделал стиль докальным. Добавил команды dev и build.
    Сделал стиль докальным. Добавил команды dev и build.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     inject = require('gulp-inject'),
     gulpSequence = require('gulp-sequence'),
+    browserSync = require('browser-sync').create(),
     gutil  = require('gulp-util');
   
 gulp.task('minify-js', function () {
-  gulp.src('src/js/*.js')
+  gulp.src('src/js/registration.js')
   .pipe(uglify())
   .pipe(rename({suffix: '.min'}))
   .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
@@ -15,7 +16,7 @@ gulp.task('minify-js', function () {
 });
 
 gulp.task('minify-css', function () {
-  gulp.src('src/css/*.css')
+  gulp.src('src/css/registration.css')
   .pipe(cssmin())
   .pipe(rename({suffix: '.min'}))
   .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
@@ -27,15 +28,42 @@ gulp.task('html-build', function () {
   .pipe(gulp.dest('build/'))
   .on('end', function () {
     gulp.src('build/index.html')
-    .pipe(inject(gulp.src('./build/js/*.min.js', {read: false}), {relative: true}))
-    .pipe(inject(gulp.src('./build/css/*.min.css', {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./build/js/registration.min.js', {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./build/css/registration.min.css', {read: false}), {relative: true}))
     .pipe(gulp.dest('build/'));
   })
 });
 
-gulp.task('production', gulpSequence(
-    "minify-js",
-    "minify-css",
-    "html-build"
-));
+gulp.task('dev', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    }
+  });
+  gulp.watch("./src/*").on('change', browserSync.reload);
+});
 
+gulp.task('build', function () {
+  function update () {
+    gulpSequence(
+      "minify-js",
+      "minify-css",
+      "html-build",
+      "html-build",
+      function () {
+        gulp.src('src/css/bootstrap.min.css').pipe(gulp.dest('build/css'));
+        gulp.src('src/js/bootstrap.min.js').pipe(gulp.dest('build/js'));
+        gulp.src('src/js/jquery-3.2.1.slim.min.js').pipe(gulp.dest('build/js'));
+        gulp.src('src/js/popper.min.js').pipe(gulp.dest('build/js'));
+        browserSync.reload
+      }
+    )
+  }
+  browserSync.init({
+    server: {
+      baseDir: "./build"
+    }
+  });
+  update ();
+  gulp.watch("./src/*").on('change', update);
+});
